fa05c87b0cbe10f6306b1f4de03ca4c6e71587b7
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-04-23 16:23:10 -0700

    Delete public module and theme directories on uninstall

--- src/Console/Uninstaller/DeleteDirectories.php
+++ src/Console/Uninstaller/DeleteDirectories.php
@@ -13,12 +13,41 @@ class DeleteDirectories
      */
     public function handle()
     {
+        $this->deleteModuleDirectories();
+        $this->deleteThemeDirectories();
+    }
+
+    /**
+     * Delete the module-specific directories used by
+     * FusionCMS.
+     *
+     * @return void
+     */
+    private function deleteModuleDirectories()
+    {
         if (File::exists(base_path('modules'))) {
             File::deleteDirectory(base_path('modules'));
         }
 
+        if (File::exists(public_path('modules'))) {
+            File::deleteDirectory(public_path('modules'));
+        }
+    }
+
+    /**
+     * Delete the theme-specific directories used by
+     * FusionCMS.
+     *
+     * @return void
+     */
+    private function deleteThemeDirectories()
+    {
         if (File::exists(base_path('themes'))) {
             File::deleteDirectory(base_path('themes'));
+        }
+
+        if (File::exists(public_path('themes'))) {
+            File::deleteDirectory(public_path('themes'));
         }
     }
 }
