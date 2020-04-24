bac10bb4973c47ac7feed14acb97853ccf656bbb
Shane Krolikowski <skrolikowski@gmail.com>
2020-04-24 22:49:11 +0000

    updating extension tests

--- tests/src/Feature/ExtensionTest.php
+++ tests/src/Feature/ExtensionTest.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use Fusion\Models\Fieldset;
 use Fusion\Models\Extension;
 use Illuminate\Support\Collection;
+use Modules\BetaModule\Models\Beta;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Auth\AuthenticationException;
@@ -19,118 +20,113 @@ class ExtensionTest extends TestCase
 {
 	use RefreshDatabase, WithFaker;
 
-    // public static function setUpBeforeClass(): void
-    // {
-    //     shell_exec('cp -r ' . dirname(__file__) . '/../Stubs/MockModule ' . dirname(__file__) . '/../../modules/MockModule');
-    //     shell_exec('composer dump-autoload');
-    // }
-
-    // public static function tearDownAfterClass(): void
-    // {
-    //     shell_exec('rm -r ' . dirname(__file__) . '/../../modules/MockModule');
-    // }
-
     public function setUp(): void
     {
         parent::setUp();
         $this->handleValidationExceptions();
 
         // --
-        $this->extension = Extension::where('handle', 'mock_module_acme')->first();
-        $this->fieldset  = $this->generateFieldset();
+        $section  = \Facades\SectionFactory::times(1)->withoutFields()->create();
+        $fieldOne = \Facades\FieldFactory::withName('Content')->withType('textarea')->withSection($section)->create();
+        $fieldTwo = \Facades\FieldFactory::withName('Profiles')->withType('user')->withSection($section)->create();
+        $fieldset = \Facades\FieldsetFactory::withName('General')->withSections(collect([$section]))->create();
+        
+        $this->extension = Extension::where('handle', 'betas')->first();
+        $this->extension->attachFieldset($fieldset);
 
-        $this->extension->attachFieldset($this->fieldset);
+        $this->model = factory(Beta::class)->create();
+        $this->model = $this->model->fresh();
     }
 
     /**
-     *
+     * @test
      * @group feature
      * @group extension
      */
     public function model_with_has_extension_trait_will_be_linked_to_extensions_table()
     {
-        $this->assertDatabaseHasTable('ext_mock_module_acme');
+        $this->assertDatabaseHasTable('ext_betas');
         $this->assertDatabaseHas('extensions', [
-            'name'   => 'MockModuleAcme',
-            'handle' => 'mock_module_acme'
+            'name'   => 'Betas',
+            'handle' => 'betas'
         ]);
     }
 
     /**
-     *
+     * @test
      * @group feature
      * @group extension
      */
     public function an_extended_model_can_have_a_fieldset_attached()
     {
-        $model = factory('Modules\MockModule\Models\Acme')->create();
-
-        $this->assertInstanceOf(Fieldset::class, $model->fieldset);
-    	$this->assertCount(2, $model->fields);
+        $this->assertInstanceOf(Fieldset::class, $this->model->fieldset);
+    	$this->assertCount(2, $this->model->fields);
     }
 
     /**
-     *
+     * @test
      * @group feature
      * @group extension
      */
     public function an_extended_model_can_have_a_fieldset_detached()
     {
-        $model = factory('Modules\MockModule\Models\Acme')->create();
-        $this->extender->detachFieldset();
+        $this->extension->detachFieldset();
 
-        $this->assertNull($model->fieldset);
-        $this->assertInstanceOf(Collection::class, $model->fields);
+        $this->assertNull($this->model->fieldset);
+        $this->assertInstanceOf(Collection::class, $this->model->fields);
     }
 
     /**
-     *
+     * @test
      * @group feature
      * @group extension
      */
     public function creating_extended_model_will_also_update_extending_fields()
     {
-        $attributes = factory('Modules\MockModule\Models\Acme')->make()->toArray();
+        $attributes = factory(Beta::class)->make()->toArray();
 
         // extending fields..
         $attributes['content']  = $this->faker->sentence;
-        $attributes['profiles'] = collect([ $this->admin->id, $this->user->id ]);
+        $attributes['profiles'] = factory('Fusion\Models\User', 2)->create()->toArray();
 
         $this
             ->be($this->admin, 'api')
-            ->post('/api/mock-module/acme', $attributes)
+            ->post('/api/beta', $attributes)
             ->assertStatus(201);
 
-        $this->assertDataBaseHas('mock_module_acme', [
+        $model = Beta::where('handle', $attributes['handle'])->first();
+
+        $this->assertDataBaseHas('betas', [
             'name'        => $attributes['name'],
             'handle'      => $attributes['handle'],
             'description' => $attributes['description'],
         ]);
 
-        $this->assertDatabaseHasTable('ext_mock_module_acme', [
+        $this->assertDatabaseHas('ext_betas', [
             'content' => $attributes['content']
         ]);
 
-        $this->assertDatabaseHasTable('users_pivot', [
-            'pivot_type' => 'Fusion\Models\Extensions\MockModuleAcme',
-            'user_id'    => $this->admin->id
+        $this->assertDatabaseHas('users_pivot', [
+            'pivot_type' => 'Fusion\Models\Extensions\Betas',
+            'pivot_id'   => $model->id,
+            'user_id'    => $attributes['profiles'][0]['id']
         ]);
 
-        $this->assertDatabaseHasTable('users_pivot', [
-            'pivot_type' => 'Fusion\Models\Extensions\MockModuleAcme',
-            'user_id'    => $this->user->id
+        $this->assertDatabaseHas('users_pivot', [
+            'pivot_type' => 'Fusion\Models\Extensions\Betas',
+            'pivot_id'   => $model->id,
+            'user_id'    => $attributes['profiles'][1]['id']
         ]);
     }
 
     /**
-     *
+     * @test
      * @group feature
      * @group extension
      */
     public function updating_extended_model_will_also_update_extending_fields()
     {
-        $model      = factory('Modules\MockModule\Models\Acme')->create();
-        $attributes = $model->toArray();
+        $attributes = $this->model->toArray();
 
         // updates..
         $attributes['name']   = 'New Name';
@@ -138,78 +134,58 @@ class ExtensionTest extends TestCase
 
         // extending fields..
         $attributes['content']  = 'New Content';
-        $attributes['profiles'] = collect([ $this->admin->toArray(), $this->user->toArray() ]);
+        $attributes['profiles'] = factory('Fusion\Models\User', 2)->create()->toArray();
 
         $this
             ->be($this->admin, 'api')
-            ->patch('/api/mock-module/acme/' . $model->id, $attributes)
+            ->patch('/api/beta/' . $this->model->id, $attributes)
             ->assertStatus(200);
 
-        $model = $model->fresh();
-
-        $this->assertDataBaseHas('mock_module_acme', [
+        $this->assertDataBaseHas('betas', [
             'name'        => $attributes['name'],
             'handle'      => $attributes['handle'],
             'description' => $attributes['description'],
         ]);
 
-        $this->assertDatabaseHasTable('ext_mock_module_acme', [
+        $this->assertDatabaseHas('ext_betas', [
             'content' => $attributes['content']
         ]);
 
-        $this->assertDatabaseHasTable('users_pivot', [
-            'pivot_type' => 'Fusion\Models\Extensions\MockModuleAcme',
-            'pivot_id'   => $model->id,
-            'user_id'    => $this->admin->id
+        $this->assertDatabaseHas('users_pivot', [
+            'pivot_type' => 'Fusion\Models\Extensions\Betas',
+            'pivot_id'   => $this->model->id,
+            'user_id'    => $attributes['profiles'][0]['id']
         ]);
 
-        $this->assertDatabaseHasTable('users_pivot', [
-            'pivot_type' => 'Fusion\Models\Extensions\MockModuleAcme',
-            'pivot_id'   => $model->id,
-            'user_id'    => $this->user->id
+        $this->assertDatabaseHas('users_pivot', [
+            'pivot_type' => 'Fusion\Models\Extensions\Betas',
+            'pivot_id'   => $this->model->id,
+            'user_id'    => $attributes['profiles'][1]['id']
         ]);
     }
 
     /**
-     *
+     * @test
      * @group feature
      * @group extension
      */
     public function extending_fields_can_be_accessed_like_any_other_attribute()
     {
-        $model      = factory('Modules\MockModule\Models\Acme')->create();
-        $attributes = $model->toArray();
+        $attributes = $this->model->toArray();
 
         // updates..
-        $attributes['name'] = 'New Name';
-
-        // extending fields..
-        $attributes['content']  = 'New Content';
-        $attributes['profiles'] = collect([[ 'id' => $this->admin->id], [ 'id' => $this->user->id ]]);
+        $attributes = [
+            'name'     => ($name = $this->faker->word),
+            'handle'   => str_handle($name),
+            'content'  => $this->faker->sentence,
+            'profiles' => factory('Fusion\Models\User', 2)->create()->toArray(),
+        ];
 
         $this
             ->be($this->admin, 'api')
-            ->patch('/api/mock-module/acme/' . $model->id, $attributes)
-            ->assertStatus(200);
+            ->patch('/api/beta/' . $this->model->id, $attributes);
 
-        $model = $model->fresh();
-
-        $this->assertEquals($model->content, $attributes['content']);
-        $this->assertEquals($model->profiles->pluck('id'), collect($attributes['profiles'])->pluck('id'));
-    }
-
-    /**
-     * Spin up new Fieldset.
-     * [HELPER]
-     * @return \Fusion\Models\Fieldset
-     */
-    private function generateFieldset()
-    {
-        $section   = \Facades\SectionFactory::times(1)->withoutFields()->create();
-        $field     = \Facades\FieldFactory::withName('Content')->withType('textarea')->withSection($section)->create();
-        $fieldTwo  = \Facades\FieldFactory::withName('Profiles')->withType('user')->withSection($section)->create();
-        $fieldset  = \Facades\FieldsetFactory::withName('General')->withSections(collect([$section]))->create();
-
-        return $fieldset;
+        $this->assertEquals($this->model->content, $attributes['content']);
+        $this->assertEquals($this->model->profiles->pluck('id'), collect($attributes['profiles'])->pluck('id'));
     }
 }
