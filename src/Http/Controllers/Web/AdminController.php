b9fee7d7b164b28b30c1cabd6c0787e2a3646e0a
Shane Krolikowski <skrolikowski@gmail.com>
2020-05-21 19:29:36 +0000

    updates

--- src/Http/Controllers/Web/AdminController.php
+++ src/Http/Controllers/Web/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 	 */
 	public function __construct()
 	{
-		$this->middleware(['role:owner','permission:cp.access']);
+		$this->middleware(['permission:cp.access']);
 	}
 
     /**
