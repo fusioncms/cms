331194b140d10469551d242bb0f3880fbf52967b
Shane Krolikowski <skrolikowski@gmail.com>
2020-06-17 23:41:27 +0000

    adding back in flags; setting db charset

--- src/Console/Installer/CreateDatabase.php
+++ src/Console/Installer/CreateDatabase.php
@@ -28,17 +28,19 @@ class CreateDatabase
      */
     public function handle()
     {
-        $driver = $this->container['db_driver'];
-        $host   = $this->container['db_host'];
-        $name   = $this->container['db_name'];
-        $user   = $this->container['db_user'];
-        $pass   = $this->container['db_pass'];
+        $driver    = $this->container['db_driver']    ?? 'mysql';
+        $host      = $this->container['db_host']      ?? 'localhost';
+        $name      = $this->container['db_name']      ?? 'fusioncms';
+        $user      = $this->container['db_user']      ?? 'root';
+        $pass      = $this->container['db_pass']      ?? 'secret';
+        $charset   = $this->container['db_charset']   ?? 'utf8';
+        $collation = $this->container['db_collation'] ?? 'utf8_general_ci';
 
         try {
             $pdo = new PDO("{$driver}:host={$host}", $user, $pass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $pdo->query("CREATE DATABASE IF NOT EXISTS {$name}");
+            $pdo->query("CREATE DATABASE IF NOT EXISTS {$name} CHARACTER SET {$charset} COLLATE {$collation}");
         } catch (PDOException $e) {
             die($e->getMessage());
         }
