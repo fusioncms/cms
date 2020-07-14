ac6e7dc6d14ba4b35e944facb42b00c4812cf530
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-07-14 10:41:59 -0700

    Fix settings layout

--- resources/js/pages/Settings/SharedForm.vue
+++ resources/js/pages/Settings/SharedForm.vue
@@ -1,30 +1,28 @@
 <template>
-	<form-container>
-        <div class="card">
-            <div class="card__body">
-				<p-tabs>
-					<p-tab v-for="(section, i1) in group.fieldset.sections" :key="i1" :name="section.name" :active="i1 === 0">
-						<div v-for="(field, i2) in gui(section.fields)" :key="i2" class="pb-5">
-			                <component
-								v-if="field.settings.component"
-								:is="field.settings.component"
-								:settings="group.settings">
-							</component>
+	<div class="card">
+		<div class="card__body">
+			<p-tabs>
+				<p-tab v-for="(section, i1) in group.fieldset.sections" :key="i1" :name="section.name" :active="i1 === 0">
+					<div v-for="(field, i2) in gui(section.fields)" :key="i2" class="pb-5">
+						<component
+							v-if="field.settings.component"
+							:is="field.settings.component"
+							:settings="group.settings">
+						</component>
 
-							<component
-								v-else
-			                    :is="field.type.id + '-fieldtype'"
-			                    :field="field"
-			                    :has-error="form.errors.has(field.handle)"
-			                    :error-message="form.errors.get(field.handle)"
-			                    v-model="form[field.handle]">
-			                </component>
-						</div>
-					</p-tab>
-				</p-tabs>
-			</div>
+						<component
+							v-else
+							:is="field.type.id + '-fieldtype'"
+							:field="field"
+							:has-error="form.errors.has(field.handle)"
+							:error-message="form.errors.get(field.handle)"
+							v-model="form[field.handle]">
+						</component>
+					</div>
+				</p-tab>
+			</p-tabs>
 		</div>
-	</form-container>
+	</div>
 </template>
 
 <script>
