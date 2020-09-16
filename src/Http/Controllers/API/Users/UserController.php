290245551277cfc887b91a48b5601883376d2ce0
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-09-16 15:08:09 -0700

    whitespace

--- src/Http/Controllers/API/Users/UserController.php
+++ src/Http/Controllers/API/Users/UserController.php
@@ -86,6 +86,7 @@ class UserController extends Controller
         // password (optional)..
         if (isset($attributes['password'])) {
             $attributes['password'] = bcrypt($attributes['password']);
+
             $user->logPasswordChange();
         }
 
