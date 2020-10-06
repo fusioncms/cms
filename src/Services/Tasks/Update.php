f8ccc597726fa28604f4b9b5215967c38f6b8e68
Shane Krolikowski <skrolikowski@gmail.com>
2020-10-06 15:00:15 -0700

    ScheduleServiceProvider (#161)
    * updates
    
    * refactor
    
    * updates
    
    * updates
    
    * revert code style

---
+++ src/Services/Tasks/Update.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Fusion\Services\Tasks;
+
+use Fusion\Facades\Version;
+
+class Update extends Task
+{
+    /**
+     * Schedule Task.
+     * 
+     * @return void
+     */
+    protected function handle()
+    {
+        $this->schedule
+            ->command('fusion:update')
+            ->daily()
+            ->timezone(setting('system.time_zone'))
+            ->withoutOverlapping()
+            ->environments(['production'])
+            ->when(function() {
+                return Version::isAutoUpdateEnabled() &&
+                       Version::hasUpdate();
+            });
+    }
+}
