90972ac1dbaed55746ab799a112d8e29e3e0fc38
Shane Krolikowski <skrolikowski@gmail.com>
2020-10-16 22:25:20 +0000

    updates

--- config/queue.php
+++ config/queue.php
@@ -14,24 +14,5 @@ return [
     */
    
 	'default' => env('QUEUE_DRIVER', 'database'),
-
-	/*
-    |--------------------------------------------------------------------------
-    | Queue Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the connection information for each server that
-    | is used by your application. A default configuration has been added
-    | for each back-end shipped with Laravel. You are free to add more.
-    |
-    */
-
-	'connections' => [
-		'database' => [
-            'driver' => 'database',
-            'table' => 'jobs',
-            'queue' => 'default',
-            'retry_after' => 90,
-        ],
-	]
+    
 ];
