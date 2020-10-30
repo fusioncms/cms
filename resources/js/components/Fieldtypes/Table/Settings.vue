1966ea0cf8465c9ba23232d1e245b0dccf3122e2
Shane Krolikowski <skrolikowski@gmail.com>
2020-10-30 16:30:39 -0700

    Fieldtype - `table` (#243)
    * updates
    
    * updates

---
+++ resources/js/components/Fieldtypes/Table/Settings.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row">
+        <ui-number-group
+            class="col w-full sm:w-1/2"
+            name="settings.initRows"
+            label="Rows"
+            help="Starting rows in a new table."
+            autocomplete="off"
+            v-model="settings.initRows || 1"
+            min="1"
+            :has-error="errors.has('settings.initRows')"
+            :error-message="errors.get('settings.initRows')">
+        </ui-number-group>
+
+        <ui-number-group
+            class="col w-full sm:w-1/2"
+            name="settings.initcols"
+            label="Columns"
+            help="Starting columns in a new table."
+            autocomplete="off"
+            v-model="settings.initCols || 1"
+            min="1"
+            :has-error="errors.has('settings.initCols')"
+            :error-message="errors.get('settings.initCols')">
+        </ui-number-group>
+    </div>
+</template>
+
+<script>
+    import fieldtype from '@/mixins/fieldtype'
+
+    export default {
+        name: 'table-fieldtype-settings',
+
+        mixins: [fieldtype]
+    }
+</script>
