d6af25521d2b28c0fc7f5cbb0958394e17f1a1c2
Shane Krolikowski <skrolikowski@gmail.com>
2020-11-03 16:08:03 -0800

    updates (#244)

--- resources/js/components/Fieldtypes/DateTime/Field.vue
+++ resources/js/components/Fieldtypes/DateTime/Field.vue
@@ -1,105 +1,78 @@
 <template>
-    <div class="form__group">
-        <label :for="field.handle" class="form__label">{{field.name}}</label>
+    <ui-field-group
+        :name="field.handle"
+        :fieldId="`${field.handle}-field`"
+        :label="field.name"
+        :required="field.required"
+        :help="field.help"
+        :hasError="hasError"
+        :errorMessage="errorMessage">
 
-        <div class="form__datetime">
-            <input :id="'flatpickr_' + field.handle"
-                class="w-full focus:outline-none"
+        <div class="input-group" :id="`flatpickr_${field.handle}`">
+            <ui-input
+                data-input
+                class="field-input field"
                 :name="field.handle"
                 :help="field.help"
                 :placeholder="field.settings.placeholder"
-                v-model="datetime"
-                >
+                :required="field.required"
+                :has-error="hasError"
+                :error-message="errorMessage"
+                :aria-required="field.required"
+                :aria-describedby="field.help"
+                v-model="model">
+            </ui-input>
 
-            <a href="#" class="form__datetime--icon" title="Select date" @click="flatpickr.open()">
+            <ui-button icon data-toggle>
                 <fa-icon icon="calendar-alt"></fa-icon>
-            </a>
-    </div>
+                <span class="sr-only">Launch date picker</span>
+            </ui-button>
 
-    <a v-if="datetime" href="#" class="datetime__clear text-sm pl-1" @click.prevent="flatpickr.clear()" title="Clear date">
-      Clear date
-    </a>
-
-    <div class="form__control--meta" v-if="field.help">
-      <div class="form__help">
-        <span v-html="field.help"></span>
-      </div>
-    </div>
-
-  </div>
+            <ui-button icon data-clear>
+                <fa-icon icon="calendar-times"></fa-icon>
+                <span class="sr-only">Clear date</span>
+            </ui-button>
+        </div>
+    </ui-field-group>
 </template>
 
 <script>
-  import flatpickr from 'flatpickr'
-  export default {
-    name: 'datetime-fieldtype',
-    data() {
-      return {
-        datetime: '',
-        flatpickr: null
-      }
-    },
-    props: {
-      field: {
-        type: Object,
-        required: true,
-      },
+    import flatpickr from 'flatpickr'
+    import FieldMixin from '@/mixins/fieldtypes/field'
 
-      value: {
-        required: false,
-        default: null,
-      }
-    },
+    export default {
+        name: 'datetime-fieldtype',
 
-    methods: {
-      emitEvent(selectedDates, dateStr, instance) {
-        this.$emit('input', this.format(dateStr, instance))
-      },
+        mixins: [FieldMixin],
+        
+        data() {
+            return {
+                inst: null
+            }
+        },
 
-      format(dateStr, instance) {
+        computed: {
+            model: {
+                get() {
+                    return this.value
+                },
 
-        if (dateStr != '') {
-          let dateObject = instance.parseDate(dateStr)
-          let formattedDate = instance.formatDate(dateObject, 'Y-m-d H:i:S')
-          return formattedDate
+                set(value) {
+                    this.$emit('input', value == '' ? null : value)
+                }
+            }
+        },
+
+        mounted() {
+            this.inst = flatpickr(`#flatpickr_${this.field.handle}`, {
+                altFormat: this.field.settings.format,
+                altInput: true,
+                dateFormat: 'Y-m-d H:i:S',
+                defaultDate: this.model,
+                enableTime: (this.field.settings.time == 1),
+                wrap: true,
+                onChange: (selected, value, inst) => this.model = value
+            })
         }
-
-        return null
-
-      },
-
-      adjustTimezone(dateStr) {
-        let localDate = new Date();
-        let minuteOffset = localDate.getTimezoneOffset();
-
-        let convertedDate = new Date(this.datetime)
-        convertedDate = new Date(convertedDate.getTime() + (minuteOffset * 60000))
-
-        return convertedDate
-      }
-    },
-
-    mounted() {
-      this.datetime = this.value
-
-      this.flatpickr = flatpickr('#flatpickr_' + this.field.handle, {
-        altInput: true,
-        enableTime: this.field.settings.time,
-        altFormat: this.field.settings.format || 'Y-m-d',
-        minuteIncrement: 1,
-        allowInput: false,
-        clickOpens: true,
-        defaultDate: this.value,
-        onChange: this.emitEvent
-      })
-
-      if (this.value) {
-        let adjustedDate = this.adjustTimezone(this.datetime)
-        let formattedDate = this.format(adjustedDate, this.flatpickr)
-        this.flatpickr.setDate(formattedDate)
-        this.emitEvent(null, this.flatpickr.latestSelectedDateObj, this.flatpickr)
-      }
-
     }
-  }
 </script>
