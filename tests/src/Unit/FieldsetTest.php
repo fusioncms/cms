8781160e2c8eb1ab8fc676d48c26f382da4703f6
Shane Krolikowski <skrolikowski@gmail.com>
2020-11-11 23:50:51 +0000

    WIP

---
+++ tests/src/Unit/FieldsetTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Fusion\Tests\Unit;
+
+use Fusion\FieldsetFactory;
+use Fusion\Models\Fieldset;
+use Fusion\Models\Field;
+use Fusion\Tests\TestCase;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+
+class FieldsetTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_fieldset_can_have_multiple_fields()
+    {
+        $fieldset = factory(Fieldset::class)->create();
+dd();
+        // $this->assertInstanceOf(Field::class, $fieldset->fields->first());
+    }
+
+    public function a_database_table_is_created_with_a_fieldset()
+    {
+        $fieldset = FieldsetFactory::withName('Contacts')->create();
+
+        $this->assertDatabaseHasTable("fx_{$fieldset->handle}");
+    }
+
+    public function the_database_table_is_renamed_when_renaming_a_fieldset()
+    {
+        $fieldset = FieldsetFactory::withName('Contacts')->create();
+
+        $this->assertDatabaseHasTable('fx_contacts');
+
+        $fieldset->name   = 'Address';
+        $fieldset->handle = 'address';
+        $fieldset->save();
+
+        $this->assertDatabaseHasTable('fx_address');
+    }
+
+    public function each_fieldset_must_have_a_unique_handle()
+    {
+        $this->expectException(QueryException::class);
+        $this->expectExceptionMessage('UNIQUE constraint failed: fieldsets.handle');
+
+        $fieldset       = factory(Fieldset::class)->create();
+        $fieldset       = $fieldset->toArray();
+        $fieldset['id'] = null;
+
+        DB::table('fieldsets')->insert($fieldset);
+    }
+}
