100b4943b26f5083de48c6c1fdfeccec9c61f0f8
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2020-11-19 12:16:00 -0800

    Clear resources

--- public/js/2862.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkfusioncms=self.webpackChunkfusioncms||[]).push([[2862],{2862:(e,s,n)=>{"use strict";n.r(s),n.d(s,{default:()=>l});const t={name:"video-fieldtype-settings"};const l=(0,n(1900).Z)(t,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null).exports}}]);
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,14 +3,20 @@ const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
 
 const production = process.env.NODE_ENV === 'production'
-const sourceMap = production ? '' : 'inline-source-map'
+const sourceMap = production ? false : 'inline-source-map'
 
 mix.setPublicPath('public')
     .js('resources/js/gravity.js', 'js')
     .vue()
     .sass('resources/scss/gravity.scss', 'css', { implementation: require('node-sass') })
+    // .extract(['vue', 'vue-loader', 'vue-router', 'vue-template-compiler', 'vuex'], 'js/vue.js')
+    // .extract(['vuedraggable', 'portal-vue', 'vue-apexcharts', 'vue-feather-icons', 'vue-head', 'vue-moment', 'vue-mq', 'vue-scrollactive', 'vue2-dropzone', '@fortawesome/vue-fontawesome'], 'js/vue-plugins.js')
+    // .extract(['tailwindcss', '@tailwindcss/ui'], 'js/tailwind.js')
+    // .extract(['@fortawesome/fontawesome-free', '@fortawesome/fontawesome-svg-core', '@fortawesome/free-brands-svg-icons', '@fortawesome/free-regular-svg-icons', '@fortawesome/free-solid-svg-icons'], 'js/fontawesome.js')
+    // .extract()
+    .version()
     .webpackConfig({
-        // devtool: sourceMap,
+        devtool: sourceMap,
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'resources/js/'),
