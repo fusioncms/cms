52242ac3f929663d76e880115bb842d71579f8db
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-01-27 10:34:03 -0800

    Create scripts migration

---
+++ database/migrations/2021_01_26_000000_create_scripts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScriptsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scripts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('code')->default('');
+            $table->string('location')->default('head'); // head, before_body, after_body, foot
+            $table->string('include_on')->default('all');
+            $table->string('include_where')->default('');
+            $table->string('type')->default('user');
+            $table->integer('order')->default(0);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scripts');
+    }
+}
