314d343dd524fb18cd281207e5e7b4337ba4d78c
Kai <shea.lewis89@gmail.com>
2021-02-02 10:53:30 -0800

    Apply fixes from StyleCI (#309)

--- src/Notifications/Backups/CleanupHasFailed.php
+++ src/Notifications/Backups/CleanupHasFailed.php
@@ -31,8 +31,8 @@ class CleanupHasFailed extends BaseNotification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
-     * 
+     * @param mixed $notifiable
+     *
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable): MailMessage
