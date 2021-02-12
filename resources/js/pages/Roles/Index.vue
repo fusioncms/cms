7901e3dc8688b55bdb9bfea885ee26b77f703a24
Shane Krolikowski <skrolikowski@gmail.com>
2021-02-11 17:23:09 -0800

    bug fix & refactor (#315)

--- resources/js/pages/Roles/Index.vue
+++ resources/js/pages/Roles/Index.vue
@@ -10,7 +10,7 @@
 
         <ui-card>
             <ui-card-body>
-                <ui-table key="roles" class="roles-table" id="roles" :endpoint="endpoint" sort-by="level" show-page-status show-page-numbers show-page-nav show-page-ends>
+                <ui-table key="roles" class="roles-table" id="roles" endpoint="/datatable/roles" sort-by="level" show-page-status show-page-numbers show-page-nav show-page-ends>
                     <template slot="name" slot-scope="table">
                         <router-link :to="{ name: 'roles.show', params: {role: table.record.id} }" v-if="$can('roles.view')">{{ table.record.name }}</router-link>
                         <span v-else>{{ table.record.name }}</span>
@@ -26,15 +26,25 @@
 
                     <template slot="actions" slot-scope="table">
                         <ui-table-actions :id="'role_' + table.record.id + '_actions'" :key="'role_' + table.record.id + '_actions'">
-                            <ui-dropdown-link :to="{ name: 'roles.show', params: {role: table.record.id} }" v-if="$can('roles.view')">View</ui-dropdown-link>
-
-                            <ui-dropdown-link v-if="! isOwner(table.record.id) && $can('roles.update', table.record.level)" @click.prevent :to="{ name: 'roles.edit', params: {role: table.record.id} }">Edit</ui-dropdown-link>
-
-                            <ui-dropdown-divider v-if="isRemovable(table.record.name) && $can('roles.delete', table.record.level)"></ui-dropdown-divider>
+                            <ui-dropdown-link
+                                v-if="$can('roles.view')"
+                                :to="{ name: 'roles.show', params: {role: table.record.id} }">
+                                View
+                            </ui-dropdown-link>
 
                             <ui-dropdown-link
-                                v-if="isRemovable(table.record.name) && $can('roles.delete', table.record.level)"
-                                @click.prevent v-modal:delete-role="table.record"
+                                v-if="! isOwner(table.record.id) && $can('roles.update', table.record.level)"
+                                @click.prevent
+                                :to="{ name: 'roles.edit', params: {role: table.record.id} }">
+                                Edit
+                            </ui-dropdown-link>
+
+                            <ui-dropdown-divider v-if="isRemovable(table.record.handle) && $can('roles.delete', table.record.level)"></ui-dropdown-divider>
+
+                            <ui-dropdown-link
+                                v-if="isRemovable(table.record.handle) && $can('roles.delete', table.record.level)"
+                                @click.prevent
+                                v-modal:delete-role="table.record"
                                 classes="danger">
                                 Delete
                             </ui-dropdown-link>
@@ -45,7 +55,7 @@
         </ui-card>
 
         <portal to="modals">
-            <ui-modal name="delete-role" title="Delete Role" v-model="showDeleteModal" no-close-button no-esc-close>
+            <ui-modal name="delete-role" title="Delete Role">
                 <template slot-scope="role">
                     <p>Please select another role to move users to.</p>
 
@@ -61,8 +71,8 @@
                 </template>
 
                 <template slot="footer" slot-scope="role">
-                    <ui-button @click="destroy(role.data.id)" variant="danger" class="ml-3" :disabled="transfer.length == 0">Delete</ui-button>
-                    <ui-button @click.prevent="cancelDestroy" variant="secondary">Cancel</ui-button>
+                    <ui-button v-modal:delete-role @click="destroy(role.data.id)" variant="danger" class="ml-3" :disabled="transfer.length == 0">Delete</ui-button>
+                    <ui-button v-modal:delete-role variant="secondary">Cancel</ui-button>
                 </template>
             </ui-modal>
         </portal>
@@ -70,7 +80,7 @@
 </template>
 
 <script>
-    import _ from 'lodash'
+    import RoleMixin   from '@/mixins/roles'
 
     export default {
         auth() {
@@ -87,14 +97,10 @@
             }
         },
 
-        mixins: [
-            require('../../mixins/roles').default
-        ],
+        mixins: [RoleMixin],
 
         data() {
             return {
-                endpoint: '/datatable/roles',
-                showDeleteModal: false,
                 transfer: '',
                 roles: [],
             }
@@ -102,7 +108,7 @@
 
         methods: {
             roleOptions(currentRole) {
-                const roles = _.filter(this.roles, (role) => currentRole != role.handle)
+                const roles = _.reject(this.roles, (role) => currentRole == role.handle)
 
                 return _.map(roles, (role) => {
                     return {
@@ -113,21 +119,14 @@
             },
 
             destroy(id) {
-                axios.delete('/api/roles/' + id, {data: {
-                    transfer: this.transfer
-                }}).then((response) => {
-                    this.transfer = ''
-                    this.showDeleteModal = false
+                axios.delete(`/api/roles/${id}`, { data: { transfer: this.transfer }})
+                    .then((response) => {
+                        this.transfer = ''
 
-                    toast('Role successfully deleted.', 'success')
+                        toast('Role successfully deleted.', 'success')
 
-                    bus().$emit('refresh-datatable-roles')
-                })
-            },
-
-            cancelDestroy() {
-                this.showDeleteModal = false
-                this.transfer = ''
+                        bus().$emit('refresh-datatable-roles')
+                    })
             },
 
             isOwner(id) {
@@ -135,29 +134,22 @@
             }
         },
 
-        beforeRouteEnter(to, from, next) {
-            getRoles((error, roles) => {
-                if (error) {
-                    next((vm) => {
-                        toast(error.toString(), 'danger')
-                    })
-                } else {
-                    next((vm) => {
-                        vm.roles = roles
-                    })
-                }
+        created() {
+            bus().$on('modal-opened', (name) => {
+                if (name == 'delete-role') { this.transfer = '' }
             })
         },
-    }
 
-    export function getRoles(callback) {
-        axios.all([
-            axios.get('/api/roles'),
-        ])
-        .then(axios.spread((roles) => {
-            roles = roles.data.data
+        beforeRouteEnter(to, from, next) {
+            axios.get('/api/roles').then((response) => {
+                next(vm => vm.roles = response.data.data)
+            }).catch((err) => {
+                next(vm => {
+                    vm.$router.push('/roles')
 
-            callback(null, roles)
-        }))
+                    toast(error.toString(), 'danger')
+                })
+            })
+        }
     }
 </script>
